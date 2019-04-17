8a7a0a0d55f0cc41f661978d6383d2981bb79297
wahhab1404 <a.wahhab1404@gmail.com>
2019-04-17 07:50:04 +0300

    homework is done

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 // turn on stop function 
-$('#stopButton').on('click','turnOnStopLight')
-
 function turnOnStopLight() {
   $(this).css('background-color', 'red')
   
     // change background color of stop light to red
+    $('#stopButton').on('click', turnOnStopLight)
+function turnOnStopLight() {
+ 
+  $('#stopLight').css('background-color','red');
+ 
     
-    
-    
+
     // changes background color of green and yellow to black
-}
+    $('#slowLight').css('background-color','black');
+    $('#goLight').css('background-color','black');
+  }
+}    
 
 // turn on slow function
-function turnOnSlowLight() { 
+ 
+  
+function turnOnSlowLight() {
+ $('#stopLight').css('background-color','black');
+ 
+ 
   // change background color of slow light to yellow
+  $('#slowLight').css('background-color','yellow');
   // changes background color of red and green to black
+  $('#goLight').css('background-color','black');
 }
 
 // turn on go function
+
 function turnOnGoLight() {
+
     // change background color of go light to green
+  
+ $('#goLight').css('background-color','green');
     // changes background color of red and yellow to black
+    $('#stopLight').css('background-color','black');
+ $('#slowLight').css('background-color','black');
 }
 
 // set stop light click event
-
+$('#stopButton').on('click','turnOnStopLight')
 // set slow light click event
+$('#slowButton').on('click', turnOnSlowLight)
 
 // set go light click event
+$('#goButton').on('click', turnOnGoLight)
 
